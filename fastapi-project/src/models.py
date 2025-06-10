cc6652e97a984ba17772d8b55476d41899bb0f5b
Kirill Pervusin <ciwbkbd@gmail.com>
2025-06-10 23:20:27 +0300

    added class user and post

--- fastapi-project/src/models.py
+++ fastapi-project/src/models.py
@@ -3,3 +3,23 @@ from pydantic import BaseModel
 class BaseResponseModel(BaseModel):
     success: bool = True
     message: str | None = None
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    posts = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(Text)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="posts")
